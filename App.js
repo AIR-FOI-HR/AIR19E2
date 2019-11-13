93f42bf8f6e927b63d298e4ba8511bab3bd06865
Yaniss <yaniss.pheron@epitech.eu>
2019-11-13 19:08:57 +0100

    Done #3

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import AppNavigator from './navigation/AppNavigator';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import { IconRegistry } from 'react-native-ui-kitten';
+
 
 export default function App() {
   return (
-    <AppNavigator />
+    <React.Fragment>
+      <IconRegistry icons={EvaIconsPack} />
+      <AppNavigator />
+    </React.Fragment>
   );
 }
--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,8 +2,12 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
 import Home from '../src/home/home';
+import SignIn from '../src/login/signIn';
+import SignUp from '../src/login/signUp';
 
 const MainNav = createStackNavigator({
+    SignIn: {screen: SignIn},
+    SignUp: {screen: SignUp},
     Home: {screen: Home},
 }, {headerMode: 'none'});
 
