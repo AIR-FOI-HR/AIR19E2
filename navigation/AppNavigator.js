5b52a1ce739ffd68b38df7ff10116c9c152f8b2a
Leriche Pierre <pierre.leriche@epitech.eu>
2019-11-27 23:42:28 +0100

    feat (add) : Create meal
    All form ready need to connect and send data to firebase.

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -4,13 +4,15 @@ import { createStackNavigator } from 'react-navigation-stack';
 import Home from '../src/home/home';
 import SignIn from '../src/login/signIn';
 import SignUp from '../src/login/signUp';
+import Create from '../src/meal/create';
 
 const MainNav = createStackNavigator({
     SignIn: {screen: SignIn},
     SignUp: {screen: SignUp},
     Home: {screen: Home},
+    Create: {screen: Create},
 }, {headerMode: 'none'});
 
 const App = createAppContainer(MainNav);
 
-export default App;  
+export default App;
