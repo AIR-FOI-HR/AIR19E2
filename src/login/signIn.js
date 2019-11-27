da935129160cf89b625d4b5de27dd72e6949fe61
Joffrey Devautour <joffrey.devautour@epitech.eu>
2019-11-27 22:18:37 +0100

    [ADD] Firebase setup #7

--- src/login/signIn.js
+++ src/login/signIn.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import { View, StyleSheet, TouchableHighlight } from 'react-native';
+import { Text as TextN } from 'react-native';
+import "firebase/auth";
+import firebase from "firebase/app";
+import firebaseConfig from "../../firebaseConfig";
+import { FirebaseAuthProvider, IfFirebaseUnAuthed } from '@react-firebase/auth';
 import { View, StyleSheet } from 'react-native';
 import { mapping } from '@eva-design/eva';
 import { light as lightTheme } from '@eva-design/eva';
 import { ApplicationProvider, Layout, Input, Icon, Text, Button } from 'react-native-ui-kitten';
 
+firebase.initializeApp(firebaseConfig);
+
 const styles = StyleSheet.create({
     input: {
-      marginHorizontal: 4,
-      width: '70%'
+        marginHorizontal: 4,
+        width: '70%'
     },
     button: {
         marginVertical: 4,
@@ -27,55 +35,68 @@ export default class SignIn extends Component {
     onChangeEmail = (email) => {
         this.setState({ email });
     }
-    
+
     onChangePassword = (password) => {
         this.setState({ password });
     };
 
     onIconPress = () => {
-      const visiblePassword = !this.state.visiblePassword;
-      this.setState({ visiblePassword });
+        const visiblePassword = !this.state.visiblePassword;
+        this.setState({ visiblePassword });
     };
 
     renderIcon = (style) => {
-      const iconName = this.state.visiblePassword ? 'eye-off' : 'eye';
-      return (
-        <Icon name={iconName}/>
-      );
+        const iconName = this.state.visiblePassword ? 'eye-off' : 'eye';
+        return (
+            <Icon name={iconName} />
+        );
     };
 
     render() {
         return (
             <ApplicationProvider mapping={mapping} theme={lightTheme}>
-                <Layout style={{height:'100%'}}>
-                    <View style={{flex: 2, alignItems: 'center', marginTop: "30%"}}>
-                        <Input
-                            style={styles.input}
-                            value={this.state.email}
-                            onChangeText={this.onChangeEmail}
-                            placeholder='email'
-                        />
-                        <Input
-                            style={styles.input}
-                            value={this.state.password}
-                            placeholder='password'
-                            icon={this.renderIcon}
-                            secureTextEntry={this.state.visiblePassword}
-                            onIconPress={this.onIconPress}
-                            onChangeText={this.onChangePassword}
-                        />
-                        <Text appearance='hint' category='c1' style={{marginTop: '5%'}}>
-                            FORGOT PASSWORD ?
-                        </Text>
-                        <Button style={styles.button} status='info'>Sign In</Button>
-                        <Text appearance='hint' style={{marginTop: '10%'}}>
-                            Don't have an account ? {" "}
-                            <Text appearance='hint' style={{textDecorationLine: 'underline'}}>
-                                SIGN UP
+                <FirebaseAuthProvider firebase={firebase} {...firebaseConfig}>
+                    <Layout style={{ height: '100%' }}>
+                        <View style={{ flex: 2, alignItems: 'center', marginTop: "30%" }}>
+                            <Input
+                                style={styles.input}
+                                value={this.state.email}
+                                onChangeText={this.onChangeEmail}
+                                placeholder='email'
+                            />
+                            <Input
+                                style={styles.input}
+                                value={this.state.password}
+                                placeholder='password'
+                                icon={this.renderIcon}
+                                secureTextEntry={this.state.visiblePassword}
+                                onIconPress={this.onIconPress}
+                                onChangeText={this.onChangePassword}
+                            />
+                            <IfFirebaseUnAuthed>
+                                <Button
+                                    onPress={() => {
+                                        const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+                                        firebase.auth().signInWithPopup(googleAuthProvider);
+                                    }}>
+                                    Sign in with Google
+                                </Button>
+                            </IfFirebaseUnAuthed>
+                            <Text appearance='hint' category='c1' style={{ marginTop: '5%' }}>
+                                FORGOT PASSWORD ?
                             </Text>
-                        </Text>
-                    </View>
-                </Layout>
+                            <Button style={styles.button} status='info' onPress={() => this.props.navigation.navigate('Home')}>
+                                Sign In
+                            </Button>
+                            <Text appearance='hint' style={{ marginTop: '10%' }}>
+                                Don't have an account ? {" "}
+                                <TextN style={{ textDecorationLine: 'underline' }} onPress={() => this.props.navigation.navigate('SignUp')}>
+                                    SIGN UP
+                                </TextN>
+                            </Text>
+                        </View>
+                    </Layout>
+                </FirebaseAuthProvider>
             </ApplicationProvider>
         )
     }
